acdd124cfc8564e7d3f136443eb9a2c811a4db76
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-06-01 22:14:23 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "🌱 Starting to seed"
+
+last_names = []
+
+5.times do
+  last_names.push(Faker::Name.unique.last_name)
+end
+
+last_names.each_with_index.map do |last_name, index|
+  household = Household.new(
+    name: last_name,
+    address_id: index + 1
+  )
+
+  household.save(validate: false)
+
+  puts "🏡 Created the #{household.name} household"
+end
+
+last_names.each_with_index.map do |last_name, index|
+  address = Address.new(
+    line1: Faker::Address.street_address,
+    line2: Faker::Address.secondary_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    postal_code: Faker::Address.zip_code,
+    country: Faker::Address.country,
+    household_id: index + 1
+  )
+
+  address.save(validate: false)
+
+  puts "📪 The #{address.household.name} household has Address.id #{address.id}"
+end
+
+last_names.each_with_index.map do |last_name, index|
+  (rand(6) + 1).times do
+    first_name = Faker::Name.first_name
+    name = first_name + " #{last_name}"
+
+    person = Person.create!(
+      name: name,
+      email_address: name.parameterize(separator: "_"),
+      household_id: index + 1,
+      password_digest: BCrypt::Password.create("dodgeviper")
+    )
+
+    puts "🙋 Added #{first_name} to the #{last_name} household"
+  end
+end
