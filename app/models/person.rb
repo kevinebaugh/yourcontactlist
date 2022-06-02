f13bb0b7c6d6dd18ed3b45b31e4940ce8bba3ee0
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-06-01 22:14:53 -0400

    Counter cache

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ApplicationRecord
   has_secure_password
 
-  belongs_to :household
+  belongs_to :household, counter_cache: true
   has_one :address, through: :household
 
   validates :email_address, presence: true, uniqueness: true
