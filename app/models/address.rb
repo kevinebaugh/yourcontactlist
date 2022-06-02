22eda82ba64d7987a77272a6384e43db7d946142
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-06-01 23:01:01 -0400

    Add staleness

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,4 +5,18 @@ class Address < ApplicationRecord
   def to_envelope
     "#{line1}\n#{line2.present? ? line2 + "\n" : nil}#{city}\n#{state}\n#{postal_code}\n#{country}"
   end
+
+  def staleness
+    seconds = Time.now - updated_at
+
+    OpenStruct.new({
+      seconds: (seconds).round,
+      minutes: (seconds / 60).round,
+      hours:   (seconds / 60 / 60).round,
+      days:    (seconds / 60 / 60 / 24).round,
+      weeks:   (seconds / 60 / 60 / 24 / 7).round,
+      months:  (seconds / 60 / 60 / 24 / 7 / 12).round,
+      years:   (seconds / 60 / 60 / 24 / 365).round
+    })
+  end
 end
