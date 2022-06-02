ef80eb685d92d9ac3149390e33197e79c0f4be61
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-06-01 22:35:03 -0400

    Add to_envelope

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :household
   has_many :people, through: :households
+
+  def to_envelope
+    "#{line1}\n#{line2.present? ? line2 + "\n" : nil}#{city}\n#{state}\n#{postal_code}\n#{country}"
+  end
 end
