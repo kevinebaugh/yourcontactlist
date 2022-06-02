d87ab74a926f604285bac97cbeacb491b98eff6f
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-06-01 20:47:42 -0400

    Get ready for auth

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ApplicationRecord
+  has_secure_password
+
   belongs_to :household
   has_one :address, through: :household
+
+  validates :email_address, presence: true, uniqueness: true
 end
