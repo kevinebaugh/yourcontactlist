e94f2b197a9e0d0a4e9354107d2caaca839bc4fc
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-06-08 10:07:17 -0400

    Person#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  
+  post "/signup", to: "people#create"
+  # post "/login", to: "sessions#create"
+  get "/me", to: "people#show"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
