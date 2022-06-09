438e988acbd8807b8d4779a7e1d9f7760584fc5a
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-06-08 21:46:19 -0400

    Persist signed-in user

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 
@@ -9,6 +9,18 @@ function App() {
   const [password, setPassword] = useState("");
   const [password_confirmation, setPasswordConfirmation] = useState("");
   const [household_id, setHouseholdId] = useState("");
+
+  useEffect(() => {
+    fetch("/me")
+      .then(response => {
+        if (response.ok) {
+          response.json()
+          .then(data => {
+            setUser(data.email_address)
+          })
+        }
+      })
+  }, []);
 
   function handleSubmit(e) {
     e.preventDefault()
@@ -37,51 +49,53 @@ function App() {
       {user ? (
         <h1>Hello, {user}!</h1>
       ) : (
-        null
+        <>
+          <h1>Sign up!</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <label htmlFor="email_address">Email address</label>
+            <input
+              type="text"
+              id="email_address"
+              value={email_address}
+              onChange={(e) => setEmailAddress(e.target.value)}
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <label htmlFor="password_confirmation">Password confirmation</label>
+            <input
+              type="password"
+              id="password_confirmation"
+              value={password_confirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+            />
+
+            <label htmlFor="household_id">Household ID</label>
+            <input
+              type="text"
+              id="household_id"
+              value={household_id}
+              onChange={(e) => setHouseholdId(e.target.value)}
+            />
+
+            <button type="submit">Sign up</button>
+          </form>
+        </>
       )}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-
-        <label htmlFor="email_address">Email address</label>
-        <input
-          type="text"
-          id="email_address"
-          value={email_address}
-          onChange={(e) => setEmailAddress(e.target.value)}
-        />
-
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <label htmlFor="password_confirmation">Password confirmation</label>
-        <input
-          type="password"
-          id="password_confirmation"
-          value={password_confirmation}
-          onChange={(e) => setPasswordConfirmation(e.target.value)}
-        />
-
-        <label htmlFor="household_id">Household ID</label>
-        <input
-          type="text"
-          id="household_id"
-          value={household_id}
-          onChange={(e) => setHouseholdId(e.target.value)}
-        />
-
-        <button type="submit">Sign up</button>
-      </form>
     </>
   );
 }
